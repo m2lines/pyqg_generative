207860a2b5202194b6ee3a01ba3a639f8ced9f17
Pavel Perezhogin <pperezhogin@gmail.com>
2022-07-14 19:38:44 -0400

    Comparison of operators to Andrew's ones. Difference in 2h harmonic

--- pyqg_generative/tools/operators.py
+++ pyqg_generative/tools/operators.py
@@ -3,6 +3,7 @@ import numpy as np
 import xarray as xr
 import pyqg
 import itertools
+from functools import wraps
 
 def xarray_to_model(arr):
     nx = len(arr.x)
@@ -28,11 +29,12 @@ def ave_lev(arr: xr.DataArray, delta):
         return arr
 
 # Decorator for adjusting input to filters
-def format_filter(func):
+def array_format(func):
     '''
     If input array is not numpy of Ny x Nx,
     do something
     '''
+    @wraps(func)
     def wrapper(X, ratio=None):
         '''
         Possible inputs:
@@ -47,11 +49,10 @@ def format_filter(func):
                 return np.stack((func(X[0,:], ratio), func(X[1,:], ratio)))
             else:
                 raise ValueError('numpy array should be 2 or 3 dimensional')
-        
-        if isinstance(X, xr.DataArray):
+        elif isinstance(X, xr.DataArray):
             dims = [dim for dim in X.dims if dim not in ['x', 'y']]
             coords = [X[dim] for dim in dims]
-            if func in [coarsegrain, cut_off]:
+            if func.__name__ in map(lambda x: x.__name__, [coarsegrain, cut_off]):
                 Y = 0*X.coarsen(x=ratio, y=ratio).mean()
             else:
                 Y = 0*X
@@ -66,7 +67,7 @@ def format_filter(func):
 # ratio is the filter width compared to the grid step
 
 # Following operators do not change resolution
-@format_filter
+@array_format
 def gcm_filter(X, ratio):
     f = gcm_filters.Filter(dx_min=1,
         filter_scale=ratio,
@@ -76,21 +77,21 @@ def gcm_filter(X, ratio):
     XX = xr.DataArray(X, dims=['y', 'x'])
     return f.apply(XX, dims=('y','x')).values
 
-@format_filter
+@array_format
 def gauss_filter(X, ratio):
     Xf = np.fft.rfftn(X)
     # let model construct grid of wavectors
     m = pyqg.QGModel(nx=X.shape[0], log_level=0)
     return np.fft.irfftn(Xf * np.exp(-m.wv**2 * (ratio*m.dx)**2 / 24))
 
-@format_filter
+@array_format
 def model_filter(X, ratio):
     Xf = np.fft.rfftn(X)
     m = pyqg.QGModel(nx=X.shape[0], log_level=0)
     return np.fft.irfftn(Xf*m.filtr)
 
 # Coarsegraining operators change resolution
-@format_filter
+@array_format
 def coarsegrain(X, ratio):
     if ratio%1 != 0:
         raise ValueError('ratio must be an integer')
@@ -100,7 +101,7 @@ def coarsegrain(X, ratio):
     XX = xr.DataArray(X, dims=['y', 'x'])
     return clean_2h(XX.coarsen(y=ratio, x=ratio).mean().values)
 
-@format_filter
+@array_format
 def cut_off(X, ratio):
     if X.shape[0]%ratio != 0:
         raise ValueError('X should be divisible on ratio')
@@ -115,7 +116,7 @@ def cut_off(X, ratio):
 
     return np.fft.irfftn(trunc)
 
-@format_filter
+@array_format
 def clean_2h(X, ratio):
     '''
     Remove frequencies which potentially
@@ -126,3 +127,12 @@ def clean_2h(X, ratio):
     Xf[n,0] = 0
     Xf[:,n] = 0
     return np.fft.irfftn(Xf)
+
+def Operator1(X, ratio):
+    return model_filter(cut_off(X, ratio))
+
+def Operator2(X, ratio):
+    return gauss_filter(cut_off(X, ratio), 2)
+
+def Operator3(X, ratio):
+    return coarsegrain(gcm_filter(X, ratio), ratio)
