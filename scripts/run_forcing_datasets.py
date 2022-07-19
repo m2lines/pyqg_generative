c24d9febf21867d9a5f031576d911bbfb779c007
Pavel Perezhogin <pperezhogin@gmail.com>
2022-07-19 14:43:06 -0400

    Regenerate datawith new rescaled forcing

--- scripts/run_forcing_datasets.py
+++ scripts/run_forcing_datasets.py
@@ -10,10 +10,10 @@ def run_simulation(folder, ens, params):
         hpc=DEFAULT_HPC._update({'ntasks': 1, 'mem': 8, 'hours': 1, 'job-name': 'SGS_'+str(ens), 'gres': 'NONE'}),
         args={'forcing': 'yes', 'ensemble_member': ens, 'pyqg_params': str(params)})
 
-folder = '/scratch/pp2681/pyqg_generative/SGS_forces_white/eddy'
+folder = '/scratch/pp2681/pyqg_generative/Reference-Default-scaled/eddy'
 for ens in range(300):
     run_simulation(folder, ens, EDDY_PARAMS.nx(256))
 
-folder = '/scratch/pp2681/pyqg_generative/SGS_forces_white/jet'
-for ens in range(300):
+folder = '/scratch/pp2681/pyqg_generative/Reference-Default-scaled/jet'
+for ens in range(25):
     run_simulation(folder, ens, JET_PARAMS.nx(256))
--- scripts/run_reference.py
+++ scripts/run_reference.py
@@ -10,10 +10,10 @@ def run_simulation(folder, ens, params):
         hpc=DEFAULT_HPC._update({'ntasks': 1, 'mem': 8, 'hours': 1, 'job-name': 'SGS_'+str(ens), 'gres': 'NONE'}),
         args={'reference': 'yes', 'ensemble_member': ens, 'pyqg_params': str(params)})
 
-folder = '/scratch/pp2681/pyqg_generative/SGS_forces_white/eddy'
+folder = '/scratch/pp2681/pyqg_generative/SGS_forces_white_scaled/eddy'
 for ens in range(10):
     run_simulation(folder, ens, EDDY_PARAMS._update({'tmax': 20*YEAR}))
 
-folder = '/scratch/pp2681/pyqg_generative/SGS_forces_white/jet'
+folder = '/scratch/pp2681/pyqg_generative/SGS_forces_white_scaled/jet'
 for ens in range(10):
     run_simulation(folder, ens, JET_PARAMS._update({'tmax': 20*YEAR}))
