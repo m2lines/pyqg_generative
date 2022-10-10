f835956065f1ea017b0a229411539eaafa148d67
Pavel Perezhogin <pperezhogin@gmail.com>
2022-10-10 12:38:31 -0400

    Time step experiments + Shenlong suggestion on binding

--- scripts/rerun_parameterized.py
+++ scripts/rerun_parameterized.py
@@ -20,42 +20,42 @@ def job_name(model, operator, resolution):
 
 N_ENS = 10 # number of ensemble members
 
-#CONFIGURATION = 'eddy';
-#MODELS_FOLDER = 'models_retrain'
-#NUM_REALIZATIONS = 5
+CONFIGURATION = 'eddy';
+MODELS_FOLDER = 'models_retrain'
+NUM_REALIZATIONS = 5
 
-CONFIGURATION = 'jet_300'; TRANSFER = 'eddy'
-MODELS_FOLDER = 'models_jet'
-NUM_REALIZATIONS = 3
+#CONFIGURATION = 'jet_300'; TRANSFER = 'eddy'
+#MODELS_FOLDER = 'models_jet'
+#NUM_REALIZATIONS = 3
 
-for resolution in [96]:
-    for operator in ['Operator1', 'Operator2']:
-        print(operator, resolution, CONFIGURATION)
-        input()
-        for folder in ['OLSModel', 'CVAERegression-None', 'MeanVarModel', 'CGANRegression']:
+for resolution in [48]:
+    for operator in ['Operator2']:
+        for folder in ['MeanVarModel', 'CGANRegression']:
+            print(operator, resolution, folder, CONFIGURATION)
+            input()
             _operator = operator+'-'+str(resolution)
             for realization in range(0,NUM_REALIZATIONS):
                 model_folder = f'/scratch/pp2681/pyqg_generative/Reference-Default-scaled/{MODELS_FOLDER}/' + _operator + '/' + folder+'-'+str(realization)
                 script_py ='/home/pp2681/pyqg_generative/pyqg_generative/tools/simulate.py'
-                for basic_params, name in zip([JET_PARAMS], ['jet']):
-                    pyqg_params = basic_params.nx(resolution)._update({'tmax': 20*YEAR})
-                    
-                    # Only white noise samplings
-                    nsteps = 1; decorrelation=0; sampling='constant'
-                    model_weight=1
-                    subfolder = name + '-' + sampling + '-' + str(decorrelation)
-                    os.system('mkdir -p ' + model_folder + '/' + subfolder)
+                for dt in [3600, 3600*2, 3600*8]:
+                    for basic_params, name in zip([EDDY_PARAMS], [f'eddy-{str(dt)}']):
+                        pyqg_params = basic_params.nx(resolution)._update({'tmax': 20*YEAR, 'dt': dt})
+                        
+                        # Only white noise samplings
+                        nsteps = 1; decorrelation=0; sampling='constant'
+                        model_weight=1
+                        subfolder = name + '-' + sampling + '-' + str(decorrelation)
+                        os.system('mkdir -p ' + model_folder + '/' + subfolder)
 
-                    hours = {32:1, 48: 2, 64: 4, 96: 10}[resolution]
-                    hpc = DEFAULT_HPC._update({'ntasks': 1, 'mem': 2, 'hours': hours, 
-                        'job-name': CONFIGURATION[0]+job_name(folder, operator, resolution),
-                        'gres': 'NONE', 'partition': 'cs',
-                        'output': f'{subfolder}/%a.out', 
-                        'error':  f'{subfolder}/%a.err',
-                        'array': f'0-{str(N_ENS-1)}',
-                        'sbatch_args': '--dependency=afterok:25768057,25768058,25768059,25768060,25768061,25768062'})
+                        hours = {32:1, 48: 2, 64: 4, 96: 10}[resolution]
+                        hpc = DEFAULT_HPC._update({'ntasks': 1, 'mem': 2, 'hours': hours, 
+                            'job-name': CONFIGURATION[0]+job_name(folder, operator, resolution),
+                            'gres': 'NONE', 'partition': 'cs',
+                            'output': f'{subfolder}/%a.out', 
+                            'error':  f'{subfolder}/%a.err',
+                            'array': f'0-{str(N_ENS-1)}'})
 
-                    args = {'parameterization': 'yes', 'ensemble_member': '$SLURM_ARRAY_TASK_ID', 'pyqg_params': pyqg_params, 
-                        'subfolder': subfolder, 'sampling': sampling, 'nsteps': nsteps, 'model_weight': model_weight}
+                        args = {'parameterization': 'yes', 'ensemble_member': '$SLURM_ARRAY_TASK_ID', 'pyqg_params': pyqg_params, 
+                            'subfolder': subfolder, 'sampling': sampling, 'nsteps': nsteps, 'model_weight': model_weight}
 
-                    run_experiment(model_folder, script_py, hpc, args, delete=False)
+                        run_experiment(model_folder, script_py, hpc, args, delete=False)
--- scripts/slurm_helpers.py
+++ scripts/slurm_helpers.py
@@ -58,7 +58,9 @@ def create_slurm(folder, script_py, hpc=DEFAULT_HPC, args=DEFAULT_ARGS):
             + '/scratch/work/public/singularity/cuda11.2.2-cudnn8-devel-ubuntu20.04.sif ' \
             + '/bin/bash -c "source /ext3/env.sh; time '
     else:
+        lines.append('mkdir -p $SLURM_TMPDIR/miopen')
         singularity = 'singularity exec --rocm ' \
+            + '--bind  $SLURM_TMPDIR/miopen:$HOME/.config/miopen/ ' \
             + '--overlay /scratch/pp2681/python-container/overlay-25GB-500K.ext3:ro ' \
             + '/scratch/work/public/singularity/rocm5.1.1-ubuntu20.04.4.sif ' \
             + '/bin/bash -c "source /ext3/env.sh; time '
