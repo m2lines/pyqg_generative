5a30813792fb9e40691095bcca6c0da058db5647
Pavel Perezhogin <pperezhogin@gmail.com>
2022-10-11 16:53:02 -0400

    Reference 40 years jet

--- scripts/run_reference.py
+++ scripts/run_reference.py
@@ -2,25 +2,29 @@ import sys
 import os
 sys.path.append('../pyqg_generative/tools/')
 
-from parameters import EDDY_PARAMS, JET_PARAMS, YEAR
+from parameters import EDDY_PARAMS, JET_PARAMS, YEAR, ANDREW_1000_STEPS
 from slurm_helpers import run_experiment, DEFAULT_HPC
 
-folder = '/scratch/pp2681/pyqg_generative/Reference-Default-scaled/eddy'
-for nc in [48, 64]:
-    for dt in [7200, 28800]:
-        print(nc, dt)
-        input()
-        subfolder = f'reference_{str(dt)}_{str(nc)}'
-        os.system('mkdir -p ' + os.path.join(folder,subfolder))
-        for ens in range(10):
-            run_experiment(folder, 
+N_ENS = 10 # number of ensemble members
+
+#PARAMS=EDDY_PARAMS
+#FOLDER='eddy'
+
+PARAMS=JET_PARAMS
+FOLDER='jet_40years'
+
+folder = f'/scratch/pp2681/pyqg_generative/Reference-Default-scaled/{FOLDER}'
+for nc in [48, 64, 96, 128, 256]:
+    subfolder = f'reference_{str(nc)}'
+    os.system('mkdir -p ' + os.path.join(folder,subfolder))
+    for ens in range(N_ENS):
+        run_experiment(
+            folder, 
             script_py='/home/pp2681/pyqg_generative/pyqg_generative/tools/simulate.py', 
-            hpc=DEFAULT_HPC._update({'ntasks': 1, 'mem': 8, 'hours': 24, 
-                'job-name': f'r{str(nc)}_{str(dt)}', 'gres': 'NONE', 'partition': 'cs',
+            hpc=DEFAULT_HPC._update({'cpus': 1, 'ntasks': 1, 'mem': 8, 'hours': 24, 
+                'gres': 'NONE', 'partition': 'cs', 'job-name': f'ref{str(nc)}',
                 'output': f'{subfolder}/out-{ens}.txt', 'error': f'{subfolder}/err-{ens}.txt'}),
             args={'reference': 'yes', 'subfolder' : subfolder, 'ensemble_member': ens, 
-                'pyqg_params': str(EDDY_PARAMS._update({'tmax': 20*YEAR, 'nx': nc, 'dt': dt}))})
-
-#folder = '/scratch/pp2681/pyqg_generative/Reference-Default-scaled/jet'
-#for ens in range(10):
-#    run_simulation(folder, ens, JET_PARAMS._update({'tmax': 20*YEAR}))
+                'pyqg_params': str(PARAMS.nx(nc)._update({'tmax': 80*YEAR, 'tavestart': 20*YEAR})),
+                'sampling_freq': 4*ANDREW_1000_STEPS}
+            )
