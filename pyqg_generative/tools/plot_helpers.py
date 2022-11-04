7e3aca64a19d2c08c881aaa3d0a1cb258ea2c7e2
Pavel Perezhogin <pperezhogin@gmail.com>
2022-11-03 23:52:52 -0400

    Figure plotters

--- pyqg_generative/tools/plot_helpers.py
+++ pyqg_generative/tools/plot_helpers.py
@@ -45,7 +45,7 @@ def default_rcParams(kw={}):
 
         'axes.formatter.limits': (-1,2),
         'axes.formatter.use_mathtext': True,
-        'axes.labelpad': -2,
+        'axes.labelpad': 0,
         'axes.titlelocation' : 'right',
         
         'savefig.bbox': 'tight',
@@ -64,20 +64,46 @@ def set_letters():
             if 'subplot' in key:
                 subplot = True
         if subplot:
-            ax.text(0.03,0.9,f'({letters[j]})', transform = ax.transAxes, fontweight='bold', fontsize=11)
+            ax.text(-0.2,1.05,f'({letters[j]})', transform = ax.transAxes, fontweight='bold', fontsize=11)
             j += 1
 
-def imshow(q, cbar=True, location='right', cbar_label=None, ax=None, cmap=None, 
-    vmax = None, aspect=True, pct=99, axes=False, interpolation='none', **kwargs):
+def latex_float(f):
+    float_str = "{0:.2g}".format(f)
+    if "e" in float_str:
+        base, exponent = float_str.split("e")
+        return r"{0} \times 10^{{{1}}}".format(base, int(exponent))
+    else:
+        return float_str
+
+def imshow(_q, cbar=True, location='right', cbar_label=None, ax=None, cmap=None, 
+    vmax = None, vmin = None, aspect=True, pct=99, axes=False, interpolation='none', normalize='False', normalize_postfix='', **kwargs):
+
+    def rms(x):
+        return float(np.sqrt(np.mean(x.astype('float64')**2)))
+    def mean(x):
+        return float(np.mean(x.astype('float64')))
+
+    if normalize != 'False':
+        if normalize == 'mean':
+            q_norm = mean(_q)
+            q_str = f'$\\mu_x={latex_float(q_norm)}$'
+        else:
+            q_norm = rms(_q)
+            q_str = '$x_{rms}='+f'{latex_float(q_norm)}$'    
+        q = _q / q_norm
+        if len(normalize_postfix) > 0:
+            q_str += f' {normalize_postfix}'
+    else:
+        q = _q
 
     if q.min() < 0:
-        cmap=cmocean.cm.balance if cmap is None else cmap
         vmax = np.percentile(np.abs(q), pct) if vmax is None else vmax
-        vmin = -vmax
+        vmin = -vmax if vmin is None else vmin
     else:
-        cmap = 'inferno' if cmap is None else cmap
         vmax = np.percentile(q, pct) if vmax is None else vmax
-        vmin = 0
+        vmin = 0 if vmin is None else vmin
+
+    cmap=cmocean.cm.balance if cmap is None else cmap
     
     kw = dict(vmin=vmin, vmax=vmax, cmap=cmap, interpolation=interpolation)
     
@@ -93,6 +119,9 @@ def imshow(q, cbar=True, location='right', cbar_label=None, ax=None, cmap=None,
     if axes:
         ax.set_xlabel('Longitude')
         ax.set_ylabel('Latitude')
+    
+    if normalize != 'False':
+        ax.text(0.05,0.85,q_str,transform = ax.transAxes, fontsize=8, bbox=dict(boxstyle='round', facecolor='white', alpha=1))
 
     if cbar:
         divider = make_axes_locatable(ax)
